9f00794cfda37b0cd95eefcd80414addea4071b2
AldoBarocio <aldoba0998@gmail.com>
2026-05-21 17:03:34 -0600

    ok

--- Proyecto_AES_Gmail 3G Pospago.py
+++ Proyecto_AES_Gmail 3G Pospago.py
@@ -29,7 +29,7 @@ print(r"""
                                                             .;+;.......................;+;:.                                                                                                                                                       
 """)
 time.sleep(3)
-#-
+
 
 def destination():
     if len(sys.argv) > 1:
